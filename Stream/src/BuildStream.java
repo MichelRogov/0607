bae43bde4cb1b955804d3f2461f75756fa9e2546
Mikhail Rogov <creynir@gmail.com>
2019-07-18 10:23:50 +0300

    Add stream api  - Distinct  - Filter and count  - ForEach vs Peek  - Limit and skip  - Reduce  - HW: write implementation for empty functions that will pass tests.

--- Stream/src/BuildStream.java
+++ Stream/src/BuildStream.java
@@ -1,25 +1,28 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BuildStream {
-    public static void main(String[] args) {
 
-        Stream<String> streamOfValues = Stream.of("a1", "b2", "c3");
+    public static void createStream(){
+        System.out.println("BuildStream start");
+        // Создание стрима из значений
+        Stream<String> streamFromValues = Stream.of("a1", "a2", "a3");
+        System.out.println("streamFromValues = " + streamFromValues.collect(Collectors.toList())); // напечатает streamFromValues = [a1, a2, a3]
 
-        streamOfValues.forEach(System.out::println);
+        // Создание стрима из массива
+        String[] array = {"a1","a2","a3"};
+        Stream<String> streamFromArrays = Arrays.stream(array);
+        // Stream<String> streamFromArrays = Stream.of(array);
+        System.out.println("streamFromArrays = " + streamFromArrays.collect(Collectors.toList())); // напечатает streamFromArrays = [a1, a2, a3]
 
-        String[] array = {"a1", "b2", "c3"};
-        Stream<String> streamOfArray = Arrays.stream(array);
-        System.out.println(streamOfArray.collect(Collectors.toList()));
+        // Создание стрима из коллекции
+        Collection<String> collection = Arrays.asList("a1", "a2", "a3");
+        Stream<String> streamFromCollection = collection.stream();
+        System.out.println("streamFromCollection = " + streamFromCollection.collect(Collectors.toList())); // напечатает streamFromCollection = [a1, a2, a3]
 
-        Collection<String> stringList = Arrays.asList("a1", "b2", "c3");
-        Stream<String> streamOfCollection = stringList.stream();
 
-        Optional<String> max = streamOfCollection.peek(System.out::println).max(String::compareTo);
-
-        if(max.isPresent()) {
-            System.out.println(max.get());
-        }
     }
+
 }
--- Stream/src/People.java
+++ Stream/src/People.java
@@ -1,7 +1,9 @@
+import java.util.Objects;
+
 public class People {
-    private String name;
-    private Integer age;
-    private Sex sex;
+    private final String name;
+    private final Integer age;
+    private final Sex sex;
 
     public People(String name, Integer age, Sex sex) {
         this.name = name;
@@ -9,36 +11,39 @@ public class People {
         this.sex = sex;
     }
 
-    @Override
-    public String toString() {
-        return "People{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                ", sex=" + sex +
-                '}';
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public Integer getAge() {
         return age;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
     public Sex getSex() {
         return sex;
     }
 
-    public void setSex(Sex sex) {
-        this.sex = sex;
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof People)) return false;
+        People people = (People) o;
+        return Objects.equals(name, people.name) &&
+                Objects.equals(age, people.age) &&
+                Objects.equals(sex, people.sex);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, sex);
     }
 }
